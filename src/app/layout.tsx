c858be8d6c92d45819b0e5fa7200b298b5dbadd1
msantoslab <msantosdev.lab@gmail.com>
2025-09-23 14:11:56 +0200

    initial project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Travel Wonders – Discover the World's Most Amazing Destinations",
+  description: "Explore unique travel experiences with Travel Wonders. Discover popular destinations, inspiring traveler stories, and exclusive tips to plan your next adventure around the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <main className="">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            {children}
+          </div>
+        </main>
+      </body>
+    </html>
+  );
+}
