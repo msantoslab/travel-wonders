7cf64011294436d9d49674fffad903bacacdcfcb
msantoslab <msantosdev.lab@gmail.com>
2025-09-25 19:47:48 +0200

    feat: add map background image to Hero component, helper class in global.css, and Hero component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Hero from "@/components/Hero/Hero";
 import Header from "@/components/Shared/Header";
 
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Header />
-    </>
+      <Hero />
+    </main>
   );
 }
