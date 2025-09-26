9ab8f58069c4b074b3a0835d8d520175d190c5b4
msantoslab <msantosdev.lab@gmail.com>
2025-09-26 17:27:05 +0200

    feat(Hero): refactor component using CSS grid to resolve complex layout and remove unnecessary divs

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,21 +1,24 @@
 
 export default function Hero(){
    return (
-      <section className="full-width bg-custom-beige bg-[url('/mapa_c.png')] bg-no-repeat bg-[position:right_center] py-7 px-8">
-         <div className="max-w-7xl min-h-[690px] mx-auto flex">
-            <div className="flex flex-col lg:flex-row lg:gap-6 justify-between">
-               <div className="w-full flex flex-col text-center justify-center pt-9 mb-8 lg:w-1/2 lg:pr-9 lg:text-left">
+      <section className="full-width bg-custom-beige bg-[url('/map.png')] bg-no-repeat bg-[position:right_center] py-7 px-8">
+         <div className="max-w-7xl mx-auto">
+            <div className="grid lg:grid-cols-5 gap-12 items-center min-h-[600px]">
+               <div className="col-span-2 text-center mt-12 lg:text-left">
                   <h1 className="text-6xl font-bold  leading-[4rem] lg:leading-[5rem] mb-5">Discover the World's <span className="text-custom-orange">Hidden</span> Wonders</h1>
                   <p className="font-semibold text-gray-800">Find unique moments and hidden gems that ignite unforgettable experiences. From rare encounters to remarkable destinations, we help you uncover the spark that turns every trip into a cherished story.</p>
                </div>
 
-               <div className="w-full lg:w-3/5 flex flex-row gap-3 lg:gap-6 items-center">
-                  <div className="">
-                     <img src="https://picsum.photos/590/760" alt="Travel Image" className="rounded-lg w-[260px] h-[360px] lg:w-[270px] lg:h-[460px]" />
+               <div className="col-span-3 grid grid-cols-2 grid-rows-[250px_210px] gap-2 lg:gap-12 lg:p-6 lg:mt-12 ">
+                  <div className="row-span-2 lg:py-8">
+                     <img src="https://picsum.photos/590/760" alt="Travel Image" className="w-full h-full object-cover rounded-lg" />
                   </div>
-                  <div className="flex flex-col gap-3 lg:gap-6">
-                     <img src="https://picsum.photos/590/760?random=1" alt="Random Travel"  className="rounded-lg w-[230px] h-[205px] lg:w-[230px] lg:h-[290px]" />
-                     <img src="https://picsum.photos/590/760?random=2" alt="Random Travel"  className="rounded-lg w-[230px] h-[145px] lg:w-[230px] lg:h-[250px]"/>
+                  
+                  <div>
+                     <img src="https://picsum.photos/590/500?random=1" alt="Random Travel"  className="w-full h-full object-cover rounded-lg" />
+                  </div>
+                  <div>
+                     <img src="https://picsum.photos/590/400?random=2" alt="Random Travel"  className="w-full h-full  object-cover rounded-lg"/>
                   </div>
                </div>
             </div>
